a50f1532348cea648069fa4f5ad6d3fb2410cc6c
Carlos D. Garza <carlos.garza@rackspace.com>
2019-05-02 16:57:22 -0500

    adding pid to row url fetcher

--- fetch_all.py
+++ fetch_all.py
@@ -2,9 +2,10 @@
 
 
 import utils
+import os
 
 printf = utils.printf
-
+printf("this pid is %s\n", os.getpid())
 rows = utils.read_csv_urls()
 bad_rows = utils.write_csv("all.csv", rows)
 printf("unable to convert %d rows\n", len(bad_rows))
